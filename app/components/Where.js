59b4ee038e5a72d7d7b33523b3f56f2d68e62831
Tanya Newell <trozova5@gmail.com>
2016-05-02 10:10:55 -0400

    modified  where component

--- app/components/Where.js
+++ app/components/Where.js
@@ -3,13 +3,13 @@ import Style from '../styles/Style';
 
 const Where = React.createClass({
   render: function(){
-    const gym = 'Brooklyn Boulders';
+    const gym = 'climbing gym';
     return(
       <div>
         <h4>Where do I climb?</h4>
           <img style={Style.img} src="/app/images/climb2.jpg" />
           <br/>
-          <form action="http://maps.google.com/maps" method="get" target="_blank">
+          <form action="https://maps.google.com/maps" method="get" target="_blank">
             <label className="googleInput" htmlFor="saddr">Enter your location:</label><br/>
             <input type="text" name="saddr" />
             <input type="hidden" name="daddr" value={gym} />
