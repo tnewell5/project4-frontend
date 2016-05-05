8d8a02cba9890f8091c0ad900725d7b870ed963c
Tanya Newell <trozova5@gmail.com>
2016-05-05 15:50:18 -0400

    updated equipment styling

--- app/components/Equipment.js
+++ app/components/Equipment.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Style from '../styles/Style';
+import {Button, Modal} from 'react-materialize';
 
 const Equipment = React.createClass({
   render: function(){
@@ -7,15 +8,34 @@ const Equipment = React.createClass({
       <div className='equipment'>
         <h4>What equipment do I need?</h4>
         <div className='equipment-images'>
-          <a className='equipment-link' href={"http://www.ems.com/search?q=climbing%20shoes"} target="blank">
-            <img style={Style.img} src="/app/images/climbing-shoes.jpg" />
-          </a>
-          <a className='equipment-link' href={"http://www.ems.com/climb/rock-climbing/chalk-bags-and-chalk/"} target="blank">
-            <img style={Style.img} src="/app/images/chalk-bag.jpg" />
-          </a>
-          <a href={"http://www.ems.com/climb/rock-climbing/harnesses/"} target="blank">
-            <img style={Style.img} src="/app/images/harness.jpg" />
-          </a>
+          <img style={Style.img} src="/app/images/climbing-shoes.jpg" />
+          <img style={Style.img} src="/app/images/chalk-bag.jpg" />
+          <img style={Style.img} src="/app/images/harness.jpg" />
+          <br/>
+          <Modal
+            header='Climbing Shoes'
+            trigger={
+              <Button className='modal-button' waves='light'>Shoes</Button>
+            }>
+            <iframe width="700" height="300" src={"http://www.ems.com/search?q=climbing%20shoes"} frameBorder="0" allowFullScreen>MODAL
+            </iframe>
+          </Modal>
+          <Modal
+            header='Chalk Bags'
+            trigger={
+              <Button className='modal-button' waves='light'>Chalk Bag</Button>
+            }>
+            <iframe width="700" height="300" src={"http://www.ems.com/climb/rock-climbing/chalk-bags-and-chalk/"} frameBorder="0" allowFullScreen>MODAL
+            </iframe>
+          </Modal>
+          <Modal
+            header='Climbing Harnesses'
+            trigger={
+              <Button className='modal-button' waves='light'>Harness</Button>
+            }>
+            <iframe width="700" height="300" src={"http://www.ems.com/climb/rock-climbing/harnesses/"} frameBorder="0" allowFullScreen>MODAL
+            </iframe>
+          </Modal>
         </div>
       </div>
     );
