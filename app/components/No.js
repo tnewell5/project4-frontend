ed2f88278848f16316eb7a7696b714792ea21a64
Tanya Newell <trozova5@gmail.com>
2016-05-06 10:41:10 -0400

    finishing touches

--- app/components/No.js
+++ app/components/No.js
@@ -4,9 +4,12 @@ import {Link} from 'react-router';
 const No = React.createClass({
   render: function(){
     return(
-      <div>
-        <h4>Come back when you will change your mind!</h4>
-        <Link to="/">Home</Link>
+      <div className='no-page'>
+        <div className='no-div'>
+          <h4>Come back when you change your mind!</h4>
+          <br/>
+          <Link to="/">Back to Home</Link>
+        </div>
       </div>
     );
   }
